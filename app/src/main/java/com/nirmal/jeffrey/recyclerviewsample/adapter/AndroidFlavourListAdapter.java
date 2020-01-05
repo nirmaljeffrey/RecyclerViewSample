78073382a3e5217fd307101263f8a808db762ade
Nirmal Jeffrey <nirmal.jeffrey@mountblue.io>
2020-01-05 14:01:15 +0530

    feat: Create activity and adapter for listAdapter

---
+++ app/src/main/java/com/nirmal/jeffrey/recyclerviewsample/adapter/AndroidFlavourListAdapter.java
@@ -0,0 +1,104 @@
+package com.nirmal.jeffrey.recyclerviewsample.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.DiffUtil.ItemCallback;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+import com.nirmal.jeffrey.recyclerviewsample.model.AndroidFlavor;
+import com.nirmal.jeffrey.recyclerviewsample.R;
+import com.nirmal.jeffrey.recyclerviewsample.adapter.AndroidFlavourListAdapter.AndroidFlavorViewHolder;
+import com.nirmal.jeffrey.recyclerviewsample.databinding.AndroidFlavorListItemBinding;
+
+public class AndroidFlavourListAdapter extends ListAdapter<AndroidFlavor, AndroidFlavorViewHolder> {
+  private AndroidFlavorClickListener flavorClickListener;
+  private static DiffUtil.ItemCallback<AndroidFlavor> DIFF_CALLBACK = new ItemCallback<AndroidFlavor>() {
+
+
+    @Nullable
+    @Override
+    public Object getChangePayload(@NonNull AndroidFlavor oldItem, @NonNull AndroidFlavor newItem) {
+      return super.getChangePayload(oldItem, newItem);
+    }
+
+    @Override
+    public boolean areItemsTheSame(@NonNull AndroidFlavor oldItem, @NonNull AndroidFlavor newItem) {
+      return oldItem.getVersionNumber().equals(newItem.getVersionNumber()) ;
+    }
+
+    @Override
+    public boolean areContentsTheSame(@NonNull AndroidFlavor oldItem,
+        @NonNull AndroidFlavor newItem) {
+      return oldItem.equals(newItem);
+    }
+  };
+
+  public AndroidFlavourListAdapter(AndroidFlavorClickListener flavorClickListener) {
+    super(DIFF_CALLBACK);
+    this.flavorClickListener = flavorClickListener;
+  }
+
+  @NonNull
+  @Override
+  public AndroidFlavorViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    AndroidFlavorListItemBinding binding = DataBindingUtil
+        .inflate(LayoutInflater.from(parent.getContext()), R.layout.android_flavor_list_item,
+            parent, false);
+    return new AndroidFlavorViewHolder(binding);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull AndroidFlavorViewHolder holder, int position) {
+    holder.bind(getItem(position));
+  }
+
+  public interface AndroidFlavorClickListener {
+    void onAndroidFlavorCardClick(AndroidFlavor flavor);
+    void onAndroidFlavorInfoClick(AndroidFlavor flavor);
+  }
+
+  public class AndroidFlavorViewHolder extends ViewHolder implements View.OnClickListener {
+
+    private AndroidFlavorListItemBinding binding;
+
+    public AndroidFlavorViewHolder(@NonNull AndroidFlavorListItemBinding binding) {
+      super(binding.getRoot());
+      this.binding = binding;
+      binding.flavorInfo.setOnClickListener(this);
+      binding.androidFlavorListItem.setOnClickListener(this);
+    }
+
+    private void bind(AndroidFlavor flavor) {
+      binding.setFlavor(flavor);
+      binding.executePendingBindings();
+    }
+
+    @Override
+    public void onClick(View view) {
+      int position = getAdapterPosition();
+
+      if (position != RecyclerView.NO_POSITION) {
+        AndroidFlavor flavor = getItem(position);
+
+        switch (view.getId()) {
+          case R.id.android_flavor_list_item: {
+            flavorClickListener.onAndroidFlavorCardClick(flavor);
+            break;
+          }
+          case R.id.flavor_info: {
+            flavorClickListener.onAndroidFlavorInfoClick(flavor);
+            break;
+          }
+        }
+      }
+    }
+
+
+  }
+}
---
+++ app/src/main/java/com/nirmal/jeffrey/recyclerviewsample/userinterface/activity/ListAdapterActivity.java
@@ -0,0 +1,79 @@
+package com.nirmal.jeffrey.recyclerviewsample.userinterface.activity;
+
+import static com.nirmal.jeffrey.recyclerviewsample.util.AndroidFlavorsUtil.FLAVOR_INFO_DIALOG_FRAGMENT_TAG;
+import static com.nirmal.jeffrey.recyclerviewsample.util.AndroidFlavorsUtil.createAndroidFlavorList;
+
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.ItemTouchHelper.SimpleCallback;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+import com.nirmal.jeffrey.recyclerviewsample.R;
+import com.nirmal.jeffrey.recyclerviewsample.adapter.AndroidFlavourListAdapter;
+import com.nirmal.jeffrey.recyclerviewsample.adapter.AndroidFlavourListAdapter.AndroidFlavorClickListener;
+import com.nirmal.jeffrey.recyclerviewsample.databinding.ActivityListAdapterBinding;
+import com.nirmal.jeffrey.recyclerviewsample.model.AndroidFlavor;
+import com.nirmal.jeffrey.recyclerviewsample.userinterface.dialog.FlavorInfoDialogFragment;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListAdapterActivity extends AppCompatActivity implements AndroidFlavorClickListener {
+private AndroidFlavourListAdapter adapter;
+private List<AndroidFlavor> flavorList;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    ActivityListAdapterBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_list_adapter);
+    setTitle(ListAdapterActivity.class.getSimpleName());
+    // If the size of each recycler view list item layout is fixed, even when the data is changed,
+    // you must call this method
+    binding.recyclerView.setHasFixedSize(true);
+    binding.recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+    flavorList = createAndroidFlavorList();
+    setUpItemTouchHelperToRecyclerView(binding.recyclerView);
+    adapter = new AndroidFlavourListAdapter(this);
+    binding.recyclerView.setAdapter(adapter);
+    adapter.submitList(flavorList);
+  }
+
+  private void setUpItemTouchHelperToRecyclerView(RecyclerView recyclerView) {
+    new ItemTouchHelper(itemTouchHelperSimpleCallBack).attachToRecyclerView(recyclerView);
+  }
+
+  private ItemTouchHelper.SimpleCallback itemTouchHelperSimpleCallBack =
+      new SimpleCallback(ItemTouchHelper.ACTION_STATE_IDLE,
+          ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull ViewHolder viewHolder,
+            @NonNull ViewHolder target) {
+          return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull ViewHolder viewHolder, int direction) {
+          int position = viewHolder.getAdapterPosition();
+          if (position != RecyclerView.NO_POSITION && adapter != null) {
+                List<AndroidFlavor> androidFlavorList = new ArrayList<>(adapter.getCurrentList());
+            androidFlavorList.remove(position);
+            adapter.submitList(androidFlavorList);
+          }
+        }
+      };
+
+  @Override
+  public void onAndroidFlavorCardClick(AndroidFlavor flavor) {
+    Toast.makeText(this, flavor.getVersionName(), Toast.LENGTH_SHORT).show();
+  }
+
+  @Override
+  public void onAndroidFlavorInfoClick(AndroidFlavor flavor) {
+    FlavorInfoDialogFragment dialog = FlavorInfoDialogFragment.getInstance(flavor);
+    dialog.show(getSupportFragmentManager(), FLAVOR_INFO_DIALOG_FRAGMENT_TAG);
+  }
+}
